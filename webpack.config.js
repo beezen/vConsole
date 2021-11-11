73a90d048663ecf24a7d9f30cd6a62cc209d450f
beezen <1184031131@qq.com>
2021-11-11 09:47:40 +0800

    移除symbol垫片和兼容css变量字段

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const pkg = require('./package.json');
-const Webpack = require('webpack');
-const Path = require('path');
-const { execSync } = require('child_process');
-const TerserPlugin = require('terser-webpack-plugin');
-const sveltePreprocess = require('svelte-preprocess');
+const pkg = require("./package.json");
+const Webpack = require("webpack");
+const Path = require("path");
+const { execSync } = require("child_process");
+const TerserPlugin = require("terser-webpack-plugin");
+const sveltePreprocess = require("svelte-preprocess");
 
 module.exports = (env, argv) => {
-  const isDev = argv.mode === 'development';
+  const isDev = argv.mode === "development";
   return {
     mode: argv.mode,
     devtool: false,
     entry: {
-      vconsole: Path.resolve(__dirname, './src/vconsole.ts'),
+      vconsole: Path.resolve(__dirname, "./src/vconsole.ts"),
     },
-    target: ['web', 'es5'],
+    target: ["web", "es5"],
     output: {
-      path: Path.resolve(__dirname, './dist'),
-      filename: '[name].min.js',
+      path: Path.resolve(__dirname, "./dist"),
+      filename: "[name].min.js",
       library: {
-        name: 'VConsole',
-        type: 'umd',
+        name: "VConsole",
+        type: "umd",
         umdNamedDefine: true,
         export: "default",
       },
     },
     resolve: {
-      extensions: ['.ts', '.js', '.html', '.less', '.mjs', '.svelte'],
+      extensions: [".ts", ".js", ".html", ".less", ".mjs", ".svelte"],
       alias: {
-        svelte: Path.resolve('node_modules', 'svelte'),
+        svelte: Path.resolve("node_modules", "svelte"),
       },
-      mainFields: ['svelte', 'browser', 'module', 'main'],
+      mainFields: ["svelte", "browser", "module", "main"],
     },
     module: {
       rules: [
         {
-          test: /\.(js|ts)$/,
-          use: [{ loader: 'babel-loader' }],
+          test: /\.(js|ts|mjs)$/,
+          use: [{ loader: "babel-loader" }],
         },
         {
           test: /\.html$/,
           // loader: 'html-loader?minimize=false'
           use: [
             {
-              loader: 'html-loader',
+              loader: "html-loader",
               options: { minimize: false },
             },
           ],
@@ -51,12 +51,20 @@ module.exports = (env, argv) => {
           test: /\.(less|css)$/i,
           // loader: 'style-loader!css-loader!less-loader'
           use: [
-            { loader: 'style-loader' },
-            { loader: 'css-loader' },
+            { loader: "style-loader" },
+            { loader: "css-loader" },
+            { loader: "postcss-loader", options: {
+              postcssOptions:{
+                plugins:[
+                  require("postcss-css-variables"),
+                  require("postcss-preset-env"),
+                ]
+              }
+            } },
             {
-              loader: 'less-loader',
+              loader: "less-loader",
               options: {
-                lessOptions: { math: 'always' },
+                lessOptions: { math: "always" },
               },
             },
           ],
@@ -64,8 +72,9 @@ module.exports = (env, argv) => {
         {
           test: /\.(svelte)$/,
           use: [
+            { loader: "babel-loader" },
             {
-              loader: 'svelte-loader',
+              loader: "svelte-loader",
               options: {
                 preprocess: sveltePreprocess({
                   sourceMap: isDev,
@@ -102,14 +111,14 @@ module.exports = (env, argv) => {
     plugins: [
       new Webpack.BannerPlugin({
         banner: [
-          'vConsole v' + pkg.version + ' (' + pkg.homepage + ')',
-          '',
-          'Tencent is pleased to support the open source community by making vConsole available.',
-          'Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.',
+          "vConsole v" + pkg.version + " (" + pkg.homepage + ")",
+          "",
+          "Tencent is pleased to support the open source community by making vConsole available.",
+          "Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.",
           'Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at',
-          'http://opensource.org/licenses/MIT',
+          "http://opensource.org/licenses/MIT",
           'Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.',
-        ].join('\n'),
+        ].join("\n"),
         entryOnly: true,
       }),
       new Webpack.DefinePlugin({
@@ -117,10 +126,10 @@ module.exports = (env, argv) => {
       }),
       {
         apply: (compiler) => {
-          compiler.hooks.done.tap('DeclarationEmitter', () => {
+          compiler.hooks.done.tap("DeclarationEmitter", () => {
             if (isDev) return; // only emit declarations in prod mode
-            console.group('Emitting type declarations...');
-            execSync('npm run build:typings');
+            console.group("Emitting type declarations...");
+            execSync("npm run build:typings");
             console.groupEnd();
           });
         },
